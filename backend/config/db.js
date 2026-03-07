e120133e75808c3ec993021b5dbe702f60b89997
Anil Jadhav <aj5388463@gmail.com>
2026-03-07 18:10:04 +0530

    fix api double path

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,17 @@
-
-import dns from 'dns';
+import dns from "dns";
 import mongoose from "mongoose";
 
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 export const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://jadhavanilsitit_db_user:ZKG3k1hkZVu7kxvH@cluster0.iwq60km.mongodb.net/?appName=Cluster0"
+      "mongodb+srv://jadhavanilsitit_db_user:ZKG3k1hkZVu7kxvH@cluster0.iwq60km.mongodb.net/expense_tracker"
     );
 
-    console.log("DB connected");
-  } catch (err) {
-    console.error("DB connection error:", err);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
   }
 };
