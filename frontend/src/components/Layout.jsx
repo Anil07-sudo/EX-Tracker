4e6ca591c9b5e4d635a01d04ec31b4b2c5c669b2
Anil Jadhav <aj5388463@gmail.com>
2026-03-07 15:13:59 +0530

    update backend API url

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -5,7 +5,8 @@ import Sidebar from './Sidebar';
 import { ArrowDown, ArrowUp, Car, ChevronDown, ChevronUp, Clock, CreditCard, DollarSign, Gift, Home, IndianRupee, Info, PieChart, PiggyBank, RefreshCw, ShoppingCart, TrendingUp, Utensils, Zap } from 'lucide-react';
 import { Outlet } from 'react-router-dom';
 import axios from "axios";
-const API_BASE = "http://localhost:4000/api";
+// const API_BASE = "http://localhost:4000/api";
+const API_BASE = "https://ex-tracker-8jy5.vercel.app/api";
 const CATEGORY_ICONS = {
   Food: <Utensils className="w-4 h-4" />,
   Housing: <Home className="w-4 h-4" />,
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -25,7 +25,7 @@ import AddTransactionModal from "../components/Add";
 
 
 
-const API_BASE = "http://localhost:4000/api";
+const API_BASE ="https://ex-tracker-8jy5.vercel.app/api";
 
 const getAuthHeader = () => {
   const token =
